3e9d5d3115e39d92df33bf83834699e9f434975c
iamtash <natashachernis@gmail.com>
2019-10-03 00:11:46 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', function(e) {
+    let newTaskField = document.getElementById('new-task-description')
+    let newTask = newTaskField.value;
+    if (newTask) {
+      e.preventDefault();
+      let taskList = document.querySelector('#list #tasks');
+      let liItem = document.createElement('li');
+      liItem.innerHTML = newTask;
+      taskList.appendChild(liItem)
+      let editButton = document.createElement('button'); editButton.innerHTML = 'Edit';
+      let deleteButton = document.createElement('button'); deleteButton.innerHTML = 'Delete';
+      liItem.appendChild(editButton); liItem.appendChild(deleteButton);
+      let itemButtons = document.querySelectorAll('#tasks li button');
+      itemButtons.forEach(function(itemButton) {
+        itemButton.addEventListener('click', function(e) {
+          if (e.srcElement.textContent === 'Edit') {
+            buildEditForm(itemButton.parentNode);
+          } else if (e.srcElement.textContent === 'Delete') {
+            itemButton.parentNode.parentNode.removeChild(itemButton.parentNode)
+          }
+        });
+      });
+      newTaskField.value = null
+      }
+  });
+
+
 });
+
+let buildEditForm = (item) => {
+  let editForm = document.createElement('form'); 
+  editForm.action = '#'; editForm.method = 'post';
+  let textInput = document.createElement('input');
+  textInput.type = 'text'; textInput.value = item.innerText.split('Edit')[0];
+  let submit = document.createElement('input'); submit.value = 'Update'
+  editForm.appendChild(textInput); editForm.appendChild(submit);
+  item.appendChild(editForm);
+}
+
